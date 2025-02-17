db547439804d8717686d4588f60ea87953020651
Anastasia Gridneva <anastasia.gridneva@mail.ru>
2025-02-17 12:39:30 +0300

    initial

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '',
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler'
+      }
+    }
+  },
+  define: {
+    'process.env': {}
+  },
+  server: {
+    port: 8080,
+  }
+})
